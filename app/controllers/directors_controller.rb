f27064ac79ffc733331c9b7f7aac1eece337d622
jacobfain <jfain@chicagobooth.edu>
2021-04-28 21:45:12 +0000

    director pages start

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,10 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/eldest.html.erb"})
   end
 
+  def director_details
+    the_id =params.fetch("an_id")
+    @the_director=Director.where({:id=>the_id}).at(0) 
+    render({:template => "director_templates/details.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+  get("/directors/:an_id", {:controller => "directors", :action =>"director_details"})
   get("/directors", {:controller => "directors", :action => "index"})
 end
