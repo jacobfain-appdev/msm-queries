64bb5614a71182b37b29f0ccc03a8b64ed136977
jacobfain <jfain@chicagobooth.edu>
2021-04-28 20:52:15 +0000

    eldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,9 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    @oldest_director = Director.where.not({:dob => nil}).order({:dob=> :asc}).at(0) 
+    render({:template => "director_templates/eldest.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
   get("/directors", {:controller => "directors", :action => "index"})
 end
